aeabe378e2864069c15601a5d11f7554f6f6c608
Alfi Noushad <Alfinoushad@gmail.com>
2025-05-29 11:38:10 +0530

    edited news com

--- src/components/Newscom.js
+++ src/components/Newscom.js
@@ -4,10 +4,24 @@ import NewsItem from './NewsItem'
 export class Newscom extends Component {
   render() {
     return (
-      <div>
-        this is the new Component
-        <NewsItem />
-      </div>
+        <div className="container my-5">
+          <h1>ABC News - HEADLINES</h1>
+          <div className="row my-5">
+            <div className="col-md-4">
+                 <NewsItem/>
+            </div>
+             <div className="col-md-4">
+                 <NewsItem/>
+            </div>
+             <div className="col-md-4">
+                 <NewsItem/>
+            </div>
+           
+          
+          </div>
+        </div>
+        
+      
     )
   }
 }
