1cf22a3773487c9174efda46ab35ff3c979fc5e0
Alfi Noushad <Alfinoushad@gmail.com>
2025-05-30 18:48:24 +0530

    class comp

--- src/components/Newscom.js
+++ src/components/Newscom.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class Newscom extends Component {
+
+ article = [
+
+ ];
+
+
+  constructor() {
+    super();
+    this.state = {
+      article: this.article,
+      loading: false
+    }
+  }
+
   render() {
     return (
         <div className="container my-5">
           <h1>ABC News - HEADLINES</h1>
           <div className="row my-5">
             <div className="col-md-4">
-                 <NewsItem/>
+                 <NewsItem title="mytitle" description="mydesc" imageUrl=""/>
             </div>
              <div className="col-md-4">
-                 <NewsItem/>
+                 <NewsItem title="mytitle" description="mydesc" imageUrl=""/>
             </div>
              <div className="col-md-4">
-                 <NewsItem/>
+                 <NewsItem title="mytitle" description="mydesc" imageUrl=""/>
             </div>
            
           
